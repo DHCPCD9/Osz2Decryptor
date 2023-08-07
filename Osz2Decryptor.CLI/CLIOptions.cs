7101015df56e1a30cb630b6d80fbac404d9481fe
DHCPCD9 <dhcpcd9@gmail.com>
2023-08-07 10:29:03 +0200

    Added cli

---
+++ Osz2Decryptor.CLI/CLIOptions.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace Osz2Decryptor.CLI;
+
+public class CLIOptions
+{
+    [Option('f', "file", Required = true, HelpText = "Path to the .osz file")]
+    public string FilePath { get; set; } = string.Empty;
+    
+    [Option('o', "output", Required = false, HelpText = "Path to the output directory")]
+    public string OutputPath { get; set; } = string.Empty;
+}
